eab2250ca8ab9eedd955960c39f9ba5b67397ec9
rss <opqlns@pm.me>
2023-03-22 16:33:07 -0500

    cleaned up css, deleted unused files, fixed img

--- app.js
+++ app.js
@@ -3,17 +3,6 @@ const sectBtns = document.querySelectorAll('.controls')
 const sectBtn = document.querySelectorAll('.control')
 const allSections = document.querySelectorAll('.main-content')
 
-// function PageTransitions(){
-//     //Button click active class
-//     for(let i = 0; i < sectBtn.length; i++){
-//         sectBtn[i].addEventListener('click', () => {
-//             let currentBtn = document.querySelectorAll('.active-btn');
-//             currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '');
-//             this.className +='active-btn'
-//         })
-//     }
-// }
-
 
 function PageTransitions () {
 
